14465d3bd3003d26c4d781227565ac0b8e26c473
dimondanyk-oss <dimon.danyk@gmail.com>
2026-09-17 12:52:25 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,9 +1,1 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
-app.get('/', (req, res) => {
-res.send('Backend is running!');
-});
-app.listen(PORT, () => {
-console.log(`Server is running on port ${PORT}`);
-});
+const express = require('express'); const path = require('path'); const app = express(); const PORT = process.env.PORT || 3000; app.get('/', (req, res) => { res.sendFile(path.join(__dirname, 'index.html')); }); app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
